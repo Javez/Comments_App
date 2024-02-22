ccc40d4b9c92291fd20fa3a69912805dd1f61f79
Алексей Данилов <mr.javez@ukr.net>
2024-02-22 19:58:26 +0200

    add initial controller files

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+const { Comment } = require("../models/comment.model");
+
+class CommentController {
+  static async addUser(req, res) {}
+
+  static async getUser(req, res) {}
+
+  static async getUsers(req, res) {}
+}
+
+export default CommentController;
