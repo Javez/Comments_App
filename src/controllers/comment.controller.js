9db46898996d8f6ee9e81b8855d9acf49ad6d04a
Алексей Данилов <mr.javez@ukr.net>
2024-02-25 13:17:31 +0200

    Fix user and comment controller to receive and send data in properly way

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,40 +5,51 @@ import commentService from "../service/comment.service.js";
 
 class CommentController {
   static async addComment(req, res) {
-    const { image, file } = req.files;
-
     try {
-      let imagePath;
-      // Process and validate image dimensions using sharp if image is uploaded as a file
-      if (image && image[0]) {
-        const imageBuffer = await sharp(image[0].buffer)
-          .resize({ width: 320, height: 240 })
-          .toBuffer();
-        imagePath = `uploads/${Date.now()}_comment_image.jpg`;
-        await sharp(imageBuffer).toFile(imagePath);
-      } else if (imageUrl) {
-        // Save directly if image is provided as a URL
-        imagePath = imageUrl;
+      let data;
+
+      if (req.files) {
+        const { image, file } = req.files;
+        let imagePath;
+        // Process and validate image dimensions using sharp if image is uploaded as a file
+        if (image && image[0]) {
+          const imageBuffer = await sharp(image[0].buffer)
+            .resize({ width: 320, height: 240 })
+            .toBuffer();
+          imagePath = `uploads/${Date.now()}_comment_image.jpg`;
+          await sharp(imageBuffer).toFile(imagePath);
+        } else if (imageUrl) {
+          // Save directly if image is provided as a URL
+          imagePath = imageUrl;
+        }
+
+        let filePath;
+        // Validate and save file path if provided
+        if (file && file[0]) {
+          filePath = `uploads/${Date.now()}_comment_file.${path.extname(
+            file[0].originalname
+          )}`;
+          await sharp(file[0].buffer).toFile(filePath);
+        }
+
+        data = {
+          userId: req.body.userId,
+          parentCommentId: req.body.parentCommentId,
+          text: req.body.text,
+          image: image,
+          file: file,
+        };
+      } else {
+        data = {
+          userId: req.body.userId,
+          parentCommentId: req.body.parentCommentId,
+          text: req.body.text,
+        };
       }
 
-      let filePath;
-      // Validate and save file path if provided
-      if (file && file[0]) {
-        filePath = `uploads/${Date.now()}_comment_file.${path.extname(
-          file[0].originalname
-        )}`;
-        await sharp(file[0].buffer).toFile(filePath);
-      }
-
-      const data = {
-        userId: req.body.userId,
-        text: req.body.text,
-        image: image,
-        file: file,
-      };
       const comment = await commentService.addComment(data);
       res.status(201).json(comment);
-    } catch {
+    } catch (error) {
       console.log(error);
       res.status(500).json({ error: error.message });
     }
@@ -53,7 +64,7 @@ class CommentController {
       };
       const comment = await commentService.addSubComment(data);
       res.status(201).json(comment);
-    } catch {
+    } catch (error) {
       console.log(error);
       res.status(500).json({ error: error.message });
     }
@@ -64,14 +75,14 @@ class CommentController {
         id: req.body.id,
       };
       res.send(await commentService.getCommentById(data));
-    } catch {
+    } catch (error) {
       console.log(error);
     }
   }
   static async getAllComments() {
     try {
       res.send(await commentService.getAllComments());
-    } catch {
+    } catch (error) {
       console.log(error);
     }
   }
@@ -81,7 +92,7 @@ class CommentController {
         parentCommentId: req.body.id,
       };
       res.send(await commentService.getSubCommentsByCommentId(data));
-    } catch {
+    } catch (error) {
       console.log(error);
     }
   }
